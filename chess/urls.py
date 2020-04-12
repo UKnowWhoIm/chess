cf8d5688c123e7f77b6ff19a89b0daba370506f1
UKnowWhoIm <51323747+UKnowWhoIm@users.noreply.github.com>
2020-04-13 03:17:59 +0530

    Renamed chess_app to chess_api

--- chess/urls.py
+++ chess/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chess/', include('chess_app.urls'))
+    path('api/', include('chess_api.urls'))
 ]
--- chess_api/apps.py
+++ chess_api/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ChessAppConfig(AppConfig):
-    name = 'chess_app'
+    name = 'chess_api'
